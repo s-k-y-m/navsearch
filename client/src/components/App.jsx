625faf75adf6b5a86ba0759ca00109706e698f28
Samuel Shih <samuels.shih@gmail.com>
2018-09-17 14:21:21 -0700

    Added dropdown

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -29,27 +29,66 @@ var fakeLocations = [
   }
 ];
 
-const App = () => {
-  return (
-    <React.Fragment>
-      <div>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nearest: 'San Francisco',
+      displayDropDown: false
+    };
+  }
+
+  displayDropDown() {
+    var count = 0;
+
+    return (
+      <React.Fragment>
+        <div className="dropDownContainer">
+          <div className="dropDownLeftContainer" />
+          <div className="dropDownDetails">
+            <div className="selectCity">- Select A City -</div>
+            {fakeLocations.map(location => {
+              count++;
+              return (
+                <DropDownDetails
+                  location={location}
+                  nearest={this.state.nearest}
+                  id={count}
+                  className="dropDownDetailsItem"
+                />
+              );
+            })}
+          </div>
+        </div>
         <div>
-          <NavBar />
+          <h1 className="placeholder" />
         </div>
-      </div>
-      <div className="dropDownContainer">
-        <div className="dropDownLeftContainer" />
-        <div className="dropDownDetails">
-          {fakeLocations.map(location => {
-            return <DropDownDetails location={location} />;
-          })}
+      </React.Fragment>
+    );
+  }
+
+  handleDropDownClicked() {
+    this.setState({
+      displayDropDown: !this.state.displayDropDown
+    });
+    console.log('Dropdown Clicked');
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <div>
+            <NavBar
+              onDropDownClicked={this.handleDropDownClicked.bind(this)}
+              displayDropDown={this.state.displayDropDown}
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <h1 className="placeholder" />
-      </div>
-    </React.Fragment>
-  );
-};
+        {this.state.displayDropDown ? this.displayDropDown() : null}
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
--- client/src/components/DropdownDetails.jsx
+++ client/src/components/DropdownDetails.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const DropDownDetails = props => {
-  const { location } = props;
+  const { location, nearest } = props;
   return (
     <div className="dropDownItem">
       <h1 className="dropDownCity">{location.city}</h1>
       <h5 className="dropDownState">{location.state}</h5>
+      {nearest === location.city ? <div class="nearest">Nearest</div> : null}
     </div>
   );
 };
